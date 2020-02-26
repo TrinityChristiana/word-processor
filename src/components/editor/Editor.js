9a4391373557aceb11beeea378a36ceb6b125390
Trinity Terry <trinitycterry@gmail.com>
2020-02-26 15:40:33 -0600

    added phase 1-3

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,13 +1,49 @@
-import React from "react";
-import "./Editor.css"
-import { Icon } from 'semantic-ui-react'
+import React, {useState} from 'react';
+import './Editor.css';
+import ContentEditable from 'react-contenteditable';
 
 const Editor = props => {
-    return (
-        <div className="editor">
-            THESE ARE words <span id="cursor">|</span>these words
-        </div>
-    )
-}
+	// const [textValue, setTextValue] = useState('');
+	const [html, setHTML] = useState('');
+	const [undoHTML, setUndoHTML] = useState([]);
+	const contentEditable = React.createRef();
+    
+	let typingTimer; //timer identifier
+
+    //user is "finished typing," do something
+    const doneTyping = (evt) => {
+        const newArray = undoHTML;
+        newArray.push(html);
+        setUndoHTML(newArray)
+        setHTML(evt.target.value)
+    }
+
+	//on keyup, start the countdown
+	const handleChange = (evt) => {
+        // console.log(evt.target.value)
+		clearTimeout(typingTimer);
+		typingTimer = setTimeout(() => doneTyping(evt), 1000);
+	};
+
+	//on keydown, clear the countdown
+	const handleKeyDown = () => {
+		clearTimeout(typingTimer);
+	}
+
+
+	// <span id="cursor"></span>
+	return (
+		<ContentEditable
+			className='editor'
+			innerRef={contentEditable}
+			html={html} // innerHTML of the editable div
+			disabled={false} // use true to disable editing
+            // onChange={getText} // handle innerHTML change
+            onKeyDown={handleKeyDown}
+            onChange={handleChange}
+			tagName='div' // Use a custom HTML tag (uses a div by default)
+		/>
+	);
+};
 
 export default Editor;
